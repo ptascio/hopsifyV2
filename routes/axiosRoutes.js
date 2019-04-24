6ef4daec22015406f9756d0000796e906552c18a
Peter Tascio <peter.tascio@gmail.com>
2019-04-23 20:23:49 -0400

    .env stuff

--- routes/axiosRoutes.js
+++ routes/axiosRoutes.js
@@ -1,4 +1,15 @@
 const axios = require("axios");
+const path = require("path");
+const dotenv = require('dotenv').config({ path: '/Users/ptascio/Documents/MyTrilogyHWs/hopsifyV2/.env' });
+console.log('this is loaded');
+console.log(dotenv);
+
+
+exports.spotify = {
+  id: process.env.SPOTIFY_KEY,
+  secret: process.env.SPOTIFY_SECRET
+};
+
 
 const axiosReq =  {
     fetchAll: () => {axios.get("https://www.google.com/")
