d7f99238d567cad9655cc7335b67d4c3e3d4db55
Peter Tascio <peter.tascio@gmail.com>
2019-04-25 06:36:06 -0400

    Add body parser

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 require("dotenv").config();
 
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 
 const port = process.env.PORT || 3000;
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 require("./controllers/trackController.js")(app);
