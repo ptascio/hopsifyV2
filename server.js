ea1edc7ff909b9b308bb3af54d7c07529a9a859c
Peter Tascio <peter.tascio@gmail.com>
2019-04-26 05:26:27 -0400

    Add concurrently

--- server.js
+++ server.js
@@ -3,11 +3,9 @@ require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
-const Vue = require("vue");
-const vm = new Vue({});
-console.log(vm);
 
-const port = process.env.PORT || 3000;
+
+const port = process.env.PORT || 3001;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
