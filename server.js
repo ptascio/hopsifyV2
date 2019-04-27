b85e25d1bdaae3bd196a573e3ee73f0d0b21f231
Peter Tascio <peter.tascio@gmail.com>
2019-04-27 06:26:39 -0400

    Try static again

--- server.js
+++ server.js
@@ -7,12 +7,11 @@ const app = express();
 
 
 const port = process.env.PORT || 3001;
-
-const staticFiles = express.static(path.join(__dirname, '../../client/build'));
-
+console.log(__dirname);
+const staticFiles = express.static(path.join(__dirname, '/client/public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(staticFiles));
+app.use(staticFiles);
 
 require("./controllers/trackController.js")(app);
 
