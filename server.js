4a0646f7e86bdada1b5c3467fc6f8ab686003ba5
Peter Tascio <peter.tascio@gmail.com>
2019-04-27 15:32:31 -0400

    Node/react env

--- server.js
+++ server.js
@@ -12,6 +12,14 @@ const port = process.env.PORT || 3001;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'client/build')));
+
+if(process.env.NODE_ENV === 'production') {
+  // app.use(express.static(path.join(__dirname, 'client/build')));
+  //
+  app.get('*', (req, res) => {
+    res.sendfile(path.join(__dirname+ 'client/build/index.html'));
+  });
+}
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname+'/client/public/index.html'));
 });
