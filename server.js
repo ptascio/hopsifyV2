11860c00e29fa0a9bc7fd9b543222946e9e7c749
Peter Tascio <peter.tascio@gmail.com>
2019-04-27 06:15:52 -0400

    Change static files directory

--- server.js
+++ server.js
@@ -2,14 +2,17 @@ require("dotenv").config();
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const path = require("path");
 const app = express();
 
 
 const port = process.env.PORT || 3001;
 
+const staticFiles = express.static(path.join(__dirname, '../../client/build'));
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static("public"));
+app.use(express.static(staticFiles));
 
 require("./controllers/trackController.js")(app);
 
