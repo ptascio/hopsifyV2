74567076f83fd15c9fe6c6f292fe7523842c7bd5
Peter Tascio <peter.tascio@gmail.com>
2019-04-27 09:31:11 -0400

    Static files stuff

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ const app = express();
 
 
 const port = process.env.PORT || 3001;
-console.log(__dirname);
-const staticFiles = express.static(path.join(__dirname, '/client/public'));
+// console.log(__dirname);
+const staticFiles = express.static(path.join(__dirname, 'client/public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(staticFiles);
