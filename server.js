a397fa342087f4d080881441fb1c3d94cd94e7e1
Peter Tascio <peter.tascio@gmail.com>
2019-04-27 21:18:50 -0400

    build again

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'client/build')));
 
 if(process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'build')));
+  app.use(express.static(path.join(__dirname, '/build')));
 
   app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, "build", 'index.html'));
