ac64f778d64fdf85f073afce6aed9837e043fd45
Peter Tascio <peter.tascio@gmail.com>
2019-04-27 21:17:02 -0400

    Maybe this build

--- server.js
+++ server.js
@@ -14,10 +14,10 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'client/build')));
 
 if(process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'client/public')));
+  app.use(express.static(path.join(__dirname, 'build')));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile(path.join(__dirname, "build", 'index.html'));
   });
 }
 app.get('*', (req, res) => {
