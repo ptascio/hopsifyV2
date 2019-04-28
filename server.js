70f36cac8e958579f6707fe4e4599e38c37f9efa
Peter Tascio <peter.tascio@gmail.com>
2019-04-27 20:44:01 -0400

    Add build script

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ if(process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, 'client/build')));
 
   app.get('*', (req, res) => {
-    res.sendfile(path.join(__dirname= 'client/build/index.html'));
+    res.sendFile(path.join(__dirname, 'client/public/index.html'));
   });
 }
 app.get('*', (req, res) => {
