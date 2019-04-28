56b66a2fd729f4444f010bdc25b4b15ed3780d6d
Peter Tascio <peter.tascio@gmail.com>
2019-04-27 20:45:26 -0400

    No such dir

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'client/build')));
 
 if(process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'client/build')));
+  app.use(express.static(path.join(__dirname, 'client/public')));
 
   app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'client/public/index.html'));
