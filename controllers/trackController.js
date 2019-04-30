48432d412270ef7a5f9512453a043f1d361d526d
Peter Tascio <peter.tascio@gmail.com>
2019-04-30 05:35:38 -0400

    Debug track search

--- controllers/trackController.js
+++ controllers/trackController.js
@@ -13,15 +13,15 @@ module.exports = (app) => {
     var artist = req.query.artistName;
     var track = req.query.trackName;
     if(spotifyToken && artist && track){
-      db.TrackSearch.create({
-        track_name: req.query.trackName,
-        artist_name: req.query.artistName
-      }).then(() => {
+      // db.TrackSearch.create({
+      //   track_name: req.query.trackName,
+      //   artist_name: req.query.artistName
+      // }).then(() => {
         axios.fetchTrackByName(artist, track, spotifyToken).then((response) => {
           console.log("Yes, in API response");
           res.send(response);
         });
-      });
+      // });
     }else{
       console.log("request failed");
       res.send("Sorry, something went wrong. Did you fill in both fields?");
