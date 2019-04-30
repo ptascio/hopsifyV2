35c461d4044fe5137bb7991b45caf6ac3ea50c52
Peter Tascio <peter.tascio@gmail.com>
2019-04-29 20:22:32 -0400

    Or in production

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 if(process.env.NODE_ENV === 'production') {
+  console.log("in production");
   app.use(express.static(path.join(__dirname, '/client/build')));
 
   app.get('*', (req, res) => {
