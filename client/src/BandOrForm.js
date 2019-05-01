292db7416940904407914fdc2ab1d12c64e4bdb9
Peter Tascio <peter.tascio@gmail.com>
2019-05-01 07:00:35 -0400

    Might need parent

---
+++ client/src/BandOrForm.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import TrackForm from "./TrackForm";
+import BandInfo from "./BandInfo";
+
+
+class BandOrForm extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {};
+  }
+
+  render(){
+    return(
+      <article>
+        HI
+      </article>
+    );
+  }
+}
