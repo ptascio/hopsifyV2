5c489d9df374c525cebc5743a227c9585de8bdb1
Peter Tascio <peter.tascio@gmail.com>
2019-05-04 20:25:23 -0400

    Start Nav component

---
+++ client/src/Nav.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+import { withRouter } from "react-router-dom";
+
+class Nav extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {};
+  }
+  rebder(){
+    return(
+      <nav>
+        <ul>
+          <li>
+            <Link to="/form">Form</Link>
+          </li>
+        </ul>
+      </nav>
+    );
+  }
+}
+
+export default withRouter(Nav);
