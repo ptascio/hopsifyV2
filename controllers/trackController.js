7b91fd2106a9250420c3aed22e19fd3ab9a40c73
Peter Tascio <peter.tascio@gmail.com>
2019-05-07 06:32:51 -0400

    Fetch from db or create entry

--- controllers/trackController.js
+++ controllers/trackController.js
@@ -23,22 +23,23 @@ module.exports = (app) => {
         axios.fetchTrackByName(artist, track, spotifyToken)
         .then((response) => {
           trackId = response.items[0].id;
-        axios.fetchTrackById(spotifyToken, "tracks", trackId)
-        .then((trackInfo) => {
-            musicObject.artistId = response.items[0].artists[0].id;
-            musicObject.songTitle = response.items[0].name;
-            musicObject.previewUrl = response.items[0].preview_url;
-            musicObject.albumImg = response.items[0].album.images[1].url;
-            axios.fetchTrackById(spotifyToken, "audio-features", trackId)
-            .then((features) => {
-              console.log(features);
-              musicObject.loudness = features.loudness;
-              musicObject.tempo = features.tempo;
-              musicObject.energy = features.energy;
-              musicObject.danceability = features.danceability;
-              res.json(musicObject);
-            });
-        });
+          musicObject.artistId = response.items[0].artists[0].id;
+          musicObject.songTitle = response.items[0].name;
+          musicObject.previewUrl = response.items[0].preview_url;
+          musicObject.albumImg = response.items[0].album.images[1].url;
+          checkDB(trackId).then(() => {
+            res.json(musicObject);
+          });
+
+            // axios.fetchTrackById(spotifyToken, "audio-features", trackId)
+            //   .then((features) => {
+            //     musicObject.loudness = features.loudness;
+            //     musicObject.tempo = features.tempo;
+            //     musicObject.energy = features.energy;
+            //     musicObject.danceability = features.danceability;
+            //     res.json(musicObject);
+            //   });
+
 
         }).catch((error) => {
           console.log("there was an error here: " + error);
@@ -49,3 +50,41 @@ module.exports = (app) => {
     }
   });
 };
+
+function checkDB(id){
+  return db.TrackInfo.findOne({
+    where: {track_id: id}
+  }).then((trackInfo) => {
+    if(!trackInfo){
+      console.log("nulllll");
+      onWard();
+    }else{
+      console.log("already here");
+      musicObject.loudness = trackInfo.dataValues.loudness;
+      musicObject.tempo = trackInfo.dataValues.tempo;
+      musicObject.energy = trackInfo.dataValues.energy;
+      musicObject.danceability = trackInfo.dataValues.danceability;
+    }
+  });
+}
+
+function onWard(){
+  axios.fetchTrackById(spotifyToken, "audio-features", trackId)
+    .then((features) => {
+      musicObject.loudness = features.loudness;
+      musicObject.tempo = features.tempo;
+      musicObject.energy = features.energy;
+      musicObject.danceability = features.danceability;
+      db.TrackInfo.create({
+        track_id: trackId,
+        loudness: features.loudness,
+        tempo: features.tempo,
+        energy: features.energy,
+        danceability: features.danceability
+      }).then((trackInfo) => {
+        console.log("created");
+      });
+    }).catch((error) => {
+      console.log("there was an error here: " + error);
+    });
+}
