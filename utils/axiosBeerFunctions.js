86569865adaac7066917af7ca1ec68522de62a70
Peter Tascio <peter.tascio@gmail.com>
2019-06-12 06:05:29 -0400

    Sketch out random beer function

--- utils/axiosBeerFunctions.js
+++ utils/axiosBeerFunctions.js
@@ -1,8 +1,18 @@
 const axios = require("axios");
 const path = require("path");
+const punkBeerUrl = "https://api.punkapi.com/v2/beers/";
 
 const axiosBeer = {
+  fetchRandomBeer: () => {
+    return axios({
+      url: punkBeerUrl+"/random",
+      method: "get"
+    }).then((response) => {
 
+    }).catch((error) => {
+      console.log(error);
+    });
+  }
 };
 
 module.exports = axiosBeer;
