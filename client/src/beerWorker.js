eedd549891697239a510f22cbff0cc96cea11605
Peter Tascio <peter.tascio@gmail.com>
2019-06-13 21:52:06 -0400

    Consider factoring in valence...

--- client/src/beerWorker.js
+++ client/src/beerWorker.js
@@ -1,3 +1,8 @@
+//energetic tracks feel fast, loud, and noisy
+//A value of 0.0 is least danceable and 1.0 is most danceable.
+//The overall loudness of a track in decibels (dB).
+//Values typical range between -60 and 0 db.
+//valence	float	A measure from 0.0 to 1.0 describing the musical positiveness conveyed by a track. Tracks with high valence sound more positive (e.g. happy, cheerful, euphoric), while tracks with low valence sound more negative (e.g. sad, depressed, angry).
 module.exports = function findBeer(loudness, tempo, energy, danceability){
   console.log(loudness);
   console.log(tempo);
