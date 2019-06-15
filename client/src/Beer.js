6ed823cbd8c816a016b017b065fcea4d7d0c0058
Peter Tascio <peter.tascio@gmail.com>
2019-06-15 14:23:22 -0400

    Get data from punk Api

--- client/src/Beer.js
+++ client/src/Beer.js
@@ -1,13 +1,24 @@
 import React from "react";
 import { Redirect, withRouter } from "react-router-dom";
-
+import axios from "axios";
+const punkAPI = "https://api.punkapi.com/v2/beers/";
 class Beer extends React.Component {
-
+  constructor(props){
+    super(props);
+    this.state = {
+      beerMatch: ""
+    };
+  }
   componentDidMount(){
     if(this.props.location.state){
       console.log("BEER PROPS");
       console.log(this.props.location.state);
-      console.log(this.props.location.state.abvPair);
+      var abv = this.props.location.state.abvPair;
+      var upperAbv = abv+1;
+      var endPoint = `${punkAPI}?per_page=1&abv_gt=${abv}&abv_lt=${upperAbv}`;
+      axios.get(endPoint).then((response) => {
+        console.log(response.data[0]);
+      });
     }else{
       console.log("false");
     }
