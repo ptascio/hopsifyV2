7e5aec35f198e5b34230941c7c6b06feff82f59b
Peter Tascio <peter.tascio@gmail.com>
2019-06-16 22:13:07 -0400

    Refactor axios call

--- client/src/Beer.js
+++ client/src/Beer.js
@@ -1,7 +1,9 @@
 import React from "react";
 import { Redirect, withRouter } from "react-router-dom";
-import axios from "axios";
+const axios = require("axios");
 const punkAPI = "https://api.punkapi.com/v2/beers/";
+
+
 class Beer extends React.Component {
   constructor(props){
     super(props);
@@ -13,18 +15,23 @@ class Beer extends React.Component {
   }
   componentDidMount(){
     if(this.props.location.state){
-      console.log("BEER PROPS");
-      console.log(this.props.location.state);
       var abv = this.props.location.state.abvPair;
       var upperAbv = abv+1;
-      var endPoint = `${punkAPI}?per_page=1&abv_gt=${abv}&abv_lt=${upperAbv}`;
-      axios.get(endPoint).then((response) => {
-        console.log(response.data[0]);
-        this.setState({
-          beerName: response.data[0].name,
-          beerImg: response.data[0].image_url,
-          beerDescription: response.data[0].description
-        });
+      // var endPoint = `${punkAPI}?per_page=3&abv_gt=${abv}&abv_lt=${upperAbv}`;
+      // axios.get(breweryDB).then((response) => {
+      //   console.log(response.data[0]);
+      //   window.beerData = response.data;
+      //   this.setState({
+      //     beerName: response.data[0].name,
+      //     beerImg: response.data[0].image_url,
+      //     beerDescription: response.data[0].description
+      //   });
+      // });
+      axios.get("/api/beer").then((resp) => {
+        console.log("in beer resp");
+        console.log(resp);
+      }).catch((err) => {
+        console.log(err);
       });
     }else{
       console.log("false");
