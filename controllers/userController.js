1d96c6849d5ec5fae8286a62e1503c595c3ffa8b
Peter Tascio <peter.tascio@gmail.com>
2019-06-23 16:43:27 -0400

    Check for dup email and username

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,30 +1,87 @@
+
+
 const db = require("../models");
 const bcrypt = require("bcrypt");
 const saltRounds = 10;
 const password = "password";
 var parsedHash;
 
+
+function checkPassword(submittedPassword, dbHash){
+  bcrypt.compare(submittedPassword, dbHash, function(err, response) {
+    if(err){
+      return err;
+    }
+    return response;
+  });
+}
+
+function hashPassword(submittedPassword){
+  bcrypt.hash(password, saltRounds, function(err, hash) {
+    if(err){
+      return;
+    }
+    return hash;
+  });
+}
+
+function checkForEmail(email){
+  db.User.findOne({email: email}).then((user) => {
+    return user;
+  }).catch((err) => {
+    return err;
+  });
+}
+
+function checkForUsername(username, cb){
+  db.User.findOne({username: "frank"}).then((user) => {
+    if(user){
+      cb("name exists");
+    }else{cb("good to go");}
+  }).catch((err) => {
+    return "err";
+  });
+}
+
 module.exports = {
   findUser: function(req, res) {
-    console.log(req.body);
+    console.log("in here");
     db.User.find({email: req.body.email})
-    .select({"email": 1, "username": 1}).then((user) => {
-      res.json(user);
+    .then((user) => {
+      console.log(user.length === 0);
+      if(checkPassword("password", user.password)){
+        console.log("inside function: " + user);
+        //res.json(user);
+      }else{
+        console.log("Login failed. Incorrect email and/or password.");
+        //res.json("Login failed. Incorrect email and/or password.");
+      }
+
     }).catch((err) => {
-      res.json(err);
+      console.log("err: " + err);
     });
+    console.log("down here");
   },
   createUser: function(req, res) {
-    bcrypt.hash(password, saltRounds, function(err, hash) {
-      parsedHash = hash;
+    db.User.create({
+      email: "email",
+      username: "frank",
+      password: "12345"
+    }).then((user) => {
+      console.log(user);
+    }).catch((err) => {
+      console.log("err: " + err);
     });
-  },
-  checkPassword: function(userpassword) {
-    bcrypt.compare(userpassword, parsedHash, function(err, response) {
-      if(err){
-        console.log("err: " + err);
-      }
-      console.log("success: " + response);
-    });
+
+
+
+
+
+
+
+
+
+
+
   }
 };
