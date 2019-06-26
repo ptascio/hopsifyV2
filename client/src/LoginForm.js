b9263e5fccbbd12d4b4c56d64d369c248326ccdf
Peter Tascio <peter.tascio@gmail.com>
2019-06-26 17:44:18 -0400

    Display error if user not found

--- client/src/LoginForm.js
+++ client/src/LoginForm.js
@@ -5,59 +5,89 @@ class LoginForm extends React.Component {
   constructor(props){
     super(props);
     this.state = {
-      email: "email",
+      email: "",
       password: "",
-      username: "frank",
+      username: "",
       notFound: "",
       currentUser: ""
     };
-    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleSubmitLogin = this.handleSubmitLogin.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmitCreateAccount = this.handleSubmitCreateAccount.bind(this);
   }
-  handleSubmit(e){
+  handleSubmitLogin(e){
     e.preventDefault();
     axios({
       url: "/api/login",
       method: "post",
       data: this.state
     }).then((response) => {
+      console.log("in here");
       this.handleResponse(response);
+    }).catch((err) => {
+      this.setState({
+        notFound: "We could not find that user. Please check email and password are correct or sign up for an account."
+      });
+    });
+  }
+
+  handleChange(e){
+    var name = e.currentTarget.name;
+    var value = e.currentTarget.value;
+    this.setState({
+      [name]: value
+    });
+
+  }
+
+  handleSubmitCreateAccount(e){
+    e.preventDefault();
+    axios({
+      url: "/api/create-account",
+      method: "post",
+      data: this.state
+    }).then((response) => {
+      console.log(response);
     });
   }
 
   handleResponse(res){
-    if(res.data.length === 0){
-      this.setState({
-        notFound: "We could not find that user. Please check email and password."
-      });
-    }else{
-      var id = res.data[0]._id;
+      console.log("here");
+      console.log(res);
+      var id = res.data._id;
       document.cookie = `userId=${id};max-age=600`;
       this.setState({
-        currentUser: res.data[0].username
+        currentUser: res.data.email
       });
 
+  }
 
+  componentDidMount(){
+    if(document.cookie){
+      console.log(this.props.history.goBack());
     }
   }
 
   render(){
-
-
     return(
       <section>
         <p>{this.state.currentUser}</p>
         <p>{this.state.notFound}</p>
-        <form onSubmit={this.handleSubmit}>
+        <p>Fill out the form:</p>
+        <form>
           <label>
             Email:
-            <input type="text" name="email"/>
+            <input onChange={this.handleChange} type="text" name="email"/>
           </label><br/>
           <label>
             Password:
-            <input type="password" name="password"/>
+            <input onChange={this.handleChange} type="password" name="password"/>
           </label><br/>
-          <input type="submit" value="Submit" />
         </form>
+        <p>Already have an account?</p>
+        <button onClick={this.handleSubmitLogin}>Login</button>
+        <p>Newbie?</p>
+        <button onClick={this.handleSubmitCreateAccount}>Create Account</button>
       </section>
     );
   }
