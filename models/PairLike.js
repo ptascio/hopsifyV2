34a5ddf6e909e744dc191b821cd67b46fa8a07bc
Peter Tascio <peter.tascio@gmail.com>
2019-07-04 06:24:12 -0400

    Create model for upvoting beer/song pairs

---
+++ models/PairLike.js
@@ -0,0 +1,26 @@
+var mongoose = require("mongoose");
+
+var Schema = mongoose.Schema;
+
+var pairLikeSchema = new Schema({
+  userId: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  beerName: {
+    type: String,
+    required: true
+  },
+  trackName: {
+    type: String,
+    required: true
+  },
+  artistId: {
+    type: String,
+    required: true
+  }
+}, {timestamps: true});
+
+var PairLike = mongoose.model("PairLike", pairLikeSchema);
+module.exports = PairLike;
