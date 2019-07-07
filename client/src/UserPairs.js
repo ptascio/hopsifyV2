51c6c9742e43611ae5e614c09a67d7afa3b1a204
Peter Tascio <peter.tascio@gmail.com>
2019-07-07 16:07:07 -0400

    Table of user pairs

--- client/src/UserPairs.js
+++ client/src/UserPairs.js
@@ -1,14 +1,54 @@
 import React from "react";
+const axios = require("axios");
 
 class UserPairs extends React.Component {
   constructor(props){
     super(props);
+    this.state = {
+      userLikes: []
+    };
+  }
+
+  componentDidMount(){
+    this.fetchPairs();
+  }
+
+  fetchPairs(){
+    var userId = window.sessionStorage.hopsifyUserId;
+    axios.get(`/api/userPairs/${userId}`).then((res) => {
+      this.setState({
+        userLikes: res.data
+      });
+    }).catch((err) => {
+      console.log(err);
+    });
   }
 
   render(){
+
+    let rows = this.state.userLikes.map((pair) => {
+      return(<tr key={pair._id} id={pair._id}>
+        <td><img src="../images/likedBottle.png" /></td>
+        <td>{pair.bandName}</td>
+        <td>{pair.trackName}</td>
+        <td>{pair.beerName}</td>
+      </tr>);
+    });
     return(
       <section>
         <h2>Your Pairs</h2>
+        <table>
+          <thead>
+          <tr>
+            <th scope="col">Band</th>
+            <th scope="col">Track</th>
+            <th scope="col">Beer</th>
+          </tr>
+        </thead>
+        <tbody>
+          {rows}
+        </tbody>
+        </table>
       </section>
     );
   }
