ca3df229b0fea70f93772a7a5a19331ae82572dc
Peter Tascio <peter.tascio@gmail.com>
2019-07-07 15:41:29 -0400

    Add user pairs component

---
+++ client/src/UserPairs.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+class UserPairs extends React.Component {
+  constructor(props){
+    super(props);
+  }
+
+  render(){
+    return(
+      <section>
+        <h2>Your Pairs</h2>
+      </section>
+    );
+  }
+}
+
+export default UserPairs;
